f72abc3f506325d104f861c9df464e389cc21866
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-05 17:27:06 +0530

    Sending data to body by postman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 app = FastAPI()
  
 @app.get("/")  #/ is the root endpoint of the API
@@ -10,5 +11,6 @@ def get_post():
     return {"message": "This is a GET request to the /post endpoint"}
 
 @app.post("/CreatePost")  #/CreatePost is the endpoint of the API
-def create_post():
-    return {"message": "This is a POST request to the /CreatePost endpoint"}
+def create_post(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"New_post":f"Title : {payLoad['title']}, Content: {payLoad['content']}"}
