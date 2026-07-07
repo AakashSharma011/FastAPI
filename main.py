85644bc24c22574efa9058e5cd7ff8b27bd726e1
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-07 09:22:26 +0530

    Adding posts to the memory

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI() #Initializing the FastAPI app
 
@@ -11,6 +12,9 @@ class Post(BaseModel): #Defining a Pydantic model for the Post data structure
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+My_posts=[{"title": "My first post", "content": "This is my first post", "ID": 1},
+                   {"title": "My second post", "content": "This is my second post", "ID": 2}]
  
 @app.get("/")  #/ is the root endpoint of the API
 def read_root():
@@ -18,9 +22,14 @@ def read_root():
 
 @app.get("/post")  #/post is the endpoint of the API
 def get_post():
-    return {"message": "This is a GET request to the /post endpoint"}
+    return {"data": My_posts}
+    #return {"message": "This is a GET request to the /post endpoint"}
 
-@app.post("/CreatePost")  #/CreatePost is the endpoint of the API
+@app.post("/CreatePost")                                 #/CreatePost is the endpoint of the API
 def create_post(new_post: Post):
-    print(new_post.rating)
-    return {"New_post": f"Title : {new_post.title}, Content: {new_post.content}"}
+    post_dict=new_post.dict()                            #Converting the Pydantic model to a dictionary
+    post_dict["ID"]=randrange(0,1000000)                 #Generating a random ID for the new post
+    My_posts.append(post_dict)                           #Appending the new post to the list of posts
+    return {"data": post_dict}                           #Returning the created post
+
+    #return {"New_post": f"Title : {new_post.title}, Content: {new_post.content}"}
