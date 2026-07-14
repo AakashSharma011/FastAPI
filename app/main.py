81ee09e74e46415d09db90c2ab8cb4ec2f79a5ce
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-14 11:10:28 +0530

    Connection to Postgres

--- app/main.py
+++ app/main.py
@@ -1,18 +1,38 @@
-#Importing the required libraries
+                       #Importing the required libraries
+
 from fastapi import FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
+                        #Initializing the FastAPI app
 
-app = FastAPI()                                         #Initializing the FastAPI app
+app = FastAPI()                                         
 
-class Post(BaseModel):                                  #Defining a Pydantic model for the Post data structure
+                        #Defining a Pydantic model for the Post data structure
+
+class Post(BaseModel):                                
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+
+                        # Connecting to the PostgreSQL database with retry logic
+
+
+while True: 
+    try:
+        conn=psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='Aakash@123', cursor_factory=RealDictCursor)
+        cursor=conn.cursor()
+        print("Database connected successfully")
+        break
+    except Exception as error:
+        print("Error while connecting to database", error)
+        time.sleep(2)                    # Wait for 2 seconds before retrying
+    
 
 My_posts=[{"title": "My first post", "content": "This is my first post", "ID": 1},
                    {"title": "My second post", "content": "This is my second post", "ID": 2}]
@@ -23,25 +43,31 @@ def read_root():
 
 @app.get("/post")                                        #/post is the endpoint of the API
 def get_posts():
-    return {"data": My_posts}
+    cursor.execute("SELECT * FROM posts")
+    post=cursor.fetchall()
+    print(post)
+    return {"data": post}
     #return {"message": "This is a GET request to the /post endpoint"}
 
 @app.post("/CreatePost")                                 #/CreatePost is the endpoint of the API
 def create_post(new_post: Post):
-    post_dict=new_post.dict()                            #Converting the Pydantic model to a dictionary
-    post_dict["ID"]=randrange(0,1000000)                 #Generating a random ID for the new post
-    My_posts.append(post_dict)  
-    raise HTTPException(status_code=status.HTTP_201_CREATED, detail="Post Successfully created")  #Raising an HTTPException with a 201 status code and a message
-    #return {"data": post_dict}                           #Returning the created post
+    cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", 
+                   (new_post.title, new_post.content, new_post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post }
+                           #Returning the created post
 
     #return {"New_post": f"Title : {new_post.title}, Content: {new_post.content}"}
 
-#Retrives last post from the list of posts
+                            #Retrives last post from the list of posts
+
 @app.get("/post/lastest")
 def get_latest_post():
     return {"data": My_posts[-1]}
 
-# Retrieve a specific post by ID
+                            # Retrieve a specific post by ID
+
 @app.get("/post/{ID}")
 def get_post(ID:int,response: Response):
     for post in My_posts:
@@ -52,7 +78,8 @@ def get_post(ID:int,response: Response):
     #return {"message": "Post not found"}
 
 
-#Delete a specific post by ID
+                            #Delete a specific post by ID
+
 @app.delete("/post/{ID}")
 def delete_post(ID:int):
     for index, post in enumerate(My_posts):
@@ -63,7 +90,8 @@ def delete_post(ID:int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID {ID} not found")
 
 
-#Update a specific post by ID
+                            #Update a specific post by ID
+
 @app.put("/post/{ID}")
 def update_post(ID:int, updated_post: Post):
     for index, post in enumerate(My_posts):
@@ -72,3 +100,4 @@ def update_post(ID:int, updated_post: Post):
             My_posts[index]["ID"] = ID  # Ensure the ID remains the same
             raise HTTPException(status_code=status.HTTP_200_OK, detail=f"Post with ID {ID} has been updated")
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID {ID} not found")
+
